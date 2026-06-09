9b98a0e4487c0e87450dbf321625cb53bc535a8e
Thomas Buri <tamasburi.jr@gmail.com>
2026-06-09 22:26:39 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage()+" test");
+        System.out.println(new App().getMessage());
     }
 
     private final String getMessage() {
